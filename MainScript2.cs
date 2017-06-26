5216afcffd6e39580cd2450e38b449a508b76b98
Robin Grattepanche <robin.grattepanche@epitech.eu>
2017-06-26 20:14:42 +0200

    [Robin] Init script Generation, Main, Network, Parsing
    envoie des scripts

---
+++ MainScript2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class MainScript2 : MonoBehaviour {
+
+    void Start () {
+    //    NetworkScript test = new NetworkScript();
+    //    test.setupSocket();
+    //    if (test.readSocket() != "")
+    //        Debug.Log("READ  " + test.readSocket());
+    //    test.writeSocket("GRAPHIC");
+    //    Debug.Log("READ  " + test.readSocket());
+        String value = "msz 200 200\n";
+        Char delim = ' ';
+        String[] substrings = value.Split(delim);
+
+        ParsingScript test = new ParsingScript();
+        test.SetServSend(substrings);
+        test.CreateDico();
+        test.SearchInDico("msz");
+
+    //  test.closeSocket();
+    }
+}
---
+++ NetworkScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+using System.Net.Sockets;
+
+public class NetworkScript : MonoBehaviour
+{
+    internal Boolean socketReady = false;
+    TcpClient mySocket;
+    NetworkStream theStream;
+    StreamWriter theWriter;
+    StreamReader theReader;
+    String Host = "10.26.111.236";
+    Int32 Port = 4242;
+    void Start()
+    {
+    }
+    void Update()
+    {
+    }
+    // **********************************************
+    public void setupSocket()
+    {
+        try
+        {
+            mySocket = new TcpClient(Host, Port);
+            theStream = mySocket.GetStream();
+            theWriter = new StreamWriter(theStream);
+            theReader = new StreamReader(theStream);
+            socketReady = true;
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Socket error: " + e);
+        }
+    }
+    public void writeSocket(string theLine)
+    {
+        if (!socketReady)
+            return;
+        String foo = theLine + "\n";
+        theWriter.Write(foo);
+        theWriter.Flush();
+    }
+    public String readSocket()
+    {
+        if (!socketReady)
+            return "";
+        if (theStream.DataAvailable)
+            return theReader.ReadLine();
+        return "";
+    }
+    public void closeSocket()
+    {
+        if (!socketReady)
+            return;
+        theWriter.Close();
+        theReader.Close();
+        mySocket.Close();
+        socketReady = false;
+    }
+} // end class s_TCP
+
+
